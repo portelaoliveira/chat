158eef0f127fc0969477221220216696181b4488
Danilo Portela <daniloportela97@gmail.com>
2023-03-23 13:31:40 -0300

    Auth Page#4.

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -13,7 +13,8 @@ class _AuthFormState extends State<AuthForm> {
   final _formData = AuthFormData();
 
   void _submit() {
-    _formKey.currentState?.validate();
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
   }
 
   @override
@@ -28,7 +29,7 @@ class _AuthFormState extends State<AuthForm> {
             children: [
               if (_formData.isSignup)
                 TextFormField(
-                  key: ValueKey('name'),
+                  key: const ValueKey('name'),
                   initialValue: _formData.name,
                   onChanged: ((name) => _formData.name = name),
                   decoration: const InputDecoration(
@@ -36,21 +37,35 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                 ),
               TextFormField(
-                key: ValueKey('email'),
+                key: const ValueKey('email'),
                 initialValue: _formData.email,
                 onChanged: ((email) => _formData.email = email),
                 decoration: const InputDecoration(
                   labelText: 'E-mail',
                 ),
+                validator: (_email) {
+                  final email = _email ?? '';
+                  if (!email.contains('@')) {
+                    return 'E-mail informado não é válido.';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
-                key: ValueKey('password'),
+                key: const ValueKey('password'),
                 initialValue: _formData.password,
                 onChanged: ((password) => _formData.password = password),
                 obscureText: true,
                 decoration: const InputDecoration(
                   labelText: 'Senha',
                 ),
+                validator: (_password) {
+                  final password = _password ?? '';
+                  if (password.length < 6) {
+                    return 'Senha deve ter no mínimo 6 caracteres.';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(
                 height: 12,
